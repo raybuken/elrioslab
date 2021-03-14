cadc0dc196954485cba7c807054a1e88816ade81
Raymundo Eduardo peña Sánchez <raymundopea00@gmail.com>
2021-03-13 23:25:56 -0400

    SEO

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,11 @@ export default function Index(){
         <div>
             <Head>
                 <title>Elrios Lab</title>
-                <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossOrigin="anonymous"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+                <meta charSet='UTF-8'/>
+                <meta name='description' content='Elsword Calculators'/>
+                <meta name='keywords' content='Elsword, Raybuken, Elsword Calculator'/>
+                <meta name='author' content='Raybuken'/>
+                <meta name='viewport' content='width=device-width' initial-scale='1.0'/>
             </Head>
             <Home/> 
         </div>
