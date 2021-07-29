64f80c94f131ed83301c025c66eab32b01be3eec
Raymundo Eduardo peña Sánchez <raymundopea00@gmail.com>
2021-07-29 08:12:26 -0400

    mov speed & jump speed fix

--- components/Tools/Stats/calc/getMultiplicativeStat.js
+++ components/Tools/Stats/calc/getMultiplicativeStat.js
@@ -7,10 +7,11 @@ const calcMultiplicativeStat = (name,actual,mStat) => {
     }
     const statName = statList[name].name
     const result = parseFloat(Number(actual) + Number(actual * mStat/100)).toFixed(2)
-    const resultAS = parseFloat( Number(actual) + (100+actual)*mStat/100 ).toFixed(2)
+    
     if( (statName === 'maximize' || statName === 'critical' ) && result > 100){
         return 100
-    }else if(statName === 'action speed'){
+    }else if(statName === 'action speed' || statName === 'movement speed' || statName === 'jump speed'){
+        const resultAS = parseFloat( Number(actual) + (100+actual)*mStat/100 ).toFixed(2)
         return resultAS
     }
     return parseFloat(result)
