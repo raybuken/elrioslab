89ee95f721fe6595d15447283f6827e8e0dfb3a8
Raymundo Eduardo peña Sánchez <raymundopea00@gmail.com>
2022-04-03 14:39:23 -0400

    search party route

---
+++ components/Tools/ElSearchPartyCollection/ElSearchPartyCollection.jsx
@@ -0,0 +1,9 @@
+
+
+export default function ElSearchPartyCollection() {
+    return(
+    <>
+    <p>Collection and sinergies</p>
+    </>
+    )
+}
---
+++ pages/calc/el-search-party-collection.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import ElSearchPartyCollection from '../../components/Tools/ElSearchPartyCollection/ElSearchPartyCollection'
+
+export default function ESPC() {
+    return(
+        <div>
+             <Head>
+                <title>El Search Party Collection</title>
+                <meta charSet='UTF-8'/>
+                <meta name='description' content='Elsword El Search Party Collection And Sinergy'/>
+                <meta name='keywords' content='Elsword, Raybuken, Elsword El Search Party Collection'/>
+                <meta name='author' content='Raybuken'/>
+                <meta name='viewport' content='width=device-width, initial-scale=1.0, shrink-to-fit=no'/>
+            </Head>
+            <Navbar/>
+            <ElSearchPartyCollection/>
+        </div>
+    )
+}
