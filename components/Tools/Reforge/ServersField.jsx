5e215441651cc33fb43e2701bc5c34b0aaadd523
Raymundo Peña <raymundopea00@gmail.com>
2024-10-05 15:22:39 -0400

    hide reforge server selector when is available only 1 server

--- components/Tools/Reforge/ServersField.jsx
+++ components/Tools/Reforge/ServersField.jsx
@@ -4,15 +4,19 @@
 function ServersField(props) {
     const {onChange, servers, defaultValue} = props
     return (
-        <>
-            <label>Server</label>
-            <select name="server" className="form-control form-select select-field" onChange={onChange} defaultValue={defaultValue}>
-                {
-                    servers.map((server,i) => (
-                        <option key={i} value={server.value}>{server.content}</option>
-                    ))   
-                }
-            </select>
+        <>  
+            {servers.length > 1 && (
+                <>
+                    <label>Server</label>
+                    <select name="server" className="form-control form-select select-field" onChange={onChange} defaultValue={defaultValue}>
+                        {
+                            servers.map((server,i) => (
+                                <option key={i} value={server.value}>{server.content}</option>
+                            ))   
+                        }
+                    </select>
+                </>
+            )}
         </>
     )
 }
