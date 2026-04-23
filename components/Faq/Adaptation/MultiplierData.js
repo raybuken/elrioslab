c59dd27d9bdd10a0531ca0802cf4bbbf8425cb85
Raymundo Peña <raymundopea00@gmail.com>
2026-04-23 00:26:13 -0400

    Include new adaptation cap. add hyperadaptation stat

--- components/Faq/Adaptation/MultiplierData.js
+++ components/Faq/Adaptation/MultiplierData.js
@@ -1,9 +1,9 @@
 import {getAtkGain} from '../../Tools/Adaptation/getAtkGain'
 
-const debuffs = [10,20,30,50,60,70,80]
+const debuffs = [10,20,30,50,60,70,80, 90, 95]
 
 const getMultiplier = debuffs.map(debuff => {
-    const result = {debuff: debuff+'%',multiplier:`${getAtkGain(debuff,debuff)}%`}
+    const result = {debuff: debuff+'%',multiplier:`${getAtkGain(debuff,debuff).atkGain}%`}
     return result
 })
 
