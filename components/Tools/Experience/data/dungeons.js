41d4afb1f9289ab431ef2619c06ad134a147ae43
Raymundo Peña <raymundopea00@gmail.com>
2026-08-27 16:19:09 -0400

    add 22-1 and 24-1 exp base

--- components/Tools/Experience/data/dungeons.js
+++ components/Tools/Experience/data/dungeons.js
@@ -1,14 +1,22 @@
 export const dungeons = [
   {
-    name: "12-8",
+    name: "12-8 (Shadow Vein)",
     baseExp: 4335323,
   },
   {
-    name: "13-3",
+    name: "13-3 (Trosh Nest)",
     baseExp: 8373706.849,
   },
   {
-    name: "16-2",
+    name: "16-2 (Plegas Reflection)",
     baseExp: 12590254,
   },
+  {
+    name: "22-1 (Magma Breath)",
+    baseExp: 22143539.154,
+  },
+  {
+    name: "24-1 (Methane Loading Dock)",
+    baseExp: 49885909.255,
+  }
 ];
